029a0c7a4d5f51ffa3b4b6443bdab934f01617a0
lantrol <landerjn@gmail.com>
2026-01-20 12:29:59 +0100

    Added some sample predicted images. Also added used script

--- src/predict.py
+++ src/predict.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 
 
-def predict_image(img_path: str = "sample/OIP.jpg"):
-    model = YOLO(model="experiments/exp001/logs/train/weights/best.pt")  # Sample model
+def predict_image(img_path: str = "sample/OIP.jpg", im_show=False):
+    model = YOLO(model="models/best.onnx", task="detect")  # Sample model
     results = model.predict(img_path)
 
     labels = model.names
@@ -15,6 +15,9 @@ def predict_image(img_path: str = "sample/OIP.jpg"):
         confs = result.boxes.conf.reshape(-1, 1)
 
         for j in range(boxes.shape[0]):
+            if confs[j] < 0.3:
+                continue
+
             preds.append(
                 (
                     boxes[j],
@@ -24,14 +27,16 @@ def predict_image(img_path: str = "sample/OIP.jpg"):
             )
 
     # Sort by x coordinate to print the number in order
+    print("Predicted reading: ")
     preds.sort(key=lambda x: x[0][0])
     for i in preds:
         print(labels[i[1].item()], end="")
     print()
 
     # Also show the image
-    results[0].show()
+    if im_show:
+        results[0].show()
 
 
 if __name__ == "__main__":
-    predict_image()
+    predict_image("sample/counter.png", im_show=True)
